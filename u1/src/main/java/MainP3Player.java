dcf92f4aeb82723f5e2f1c2a5b06caa58436ca90
Constanze <Constanze.becker@stud.uni-due.de>
2017-05-21 20:30:55 +0200

    Fxml Datei umbenannt

--- u1/src/main/java/MainP3Player.java
+++ u1/src/main/java/MainP3Player.java
@@ -1,14 +1,8 @@
-import classes.SongClass;
-import classes.SongListC;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainP3Player extends Application {
@@ -16,7 +10,7 @@ public class MainP3Player extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("fpt-uebung-1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
